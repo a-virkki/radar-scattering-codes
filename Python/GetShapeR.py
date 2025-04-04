7d6d5fd48ceca03397c617c05539397f1e7eabe1
a-virkki <54715322+a-virkki@users.noreply.github.com>
2025-04-04 17:24:31 +0300

    Update GetShapeR.py

--- Python/GetShapeR.py
+++ Python/GetShapeR.py
@@ -22,21 +22,21 @@ a0 = 14.0       # Size: a0 is the longest axis dimension. Use 0 to not scale the
 b0, c0 = 0, 0   # Intermediate and short axis dimensions. Use 0 to not scale the model size.
 # Give either the subradar latitude or the set of observer and polar ecliptic coordinates 
 # (make delta False for the latter! Note: 0 = False so use e.g. 0.01 for an equatorial view!)
-delta = 0					# subradar latitude ([-90,90])
-ObsEcLat = 35.0 #-20.3           # Asteroid's observer-centered ecliptic latitude
-ObsEcLon = 324.0          		# Asteroid's observer-centered ecliptic longitude
-PoleEcLat = -44.3 #-88.0			# Pole ecliptic latitude in degrees
-PoleEcLon = 35.6 #45				# Pole ecliptic longitude in degrees
-x0 = 10 						# Map dimension (number of pixels per side)
-law = 'Cos'						# Scattering law: 'Cos'/'Hag' for Cosine/Hagfors
-C = 0.77 						# scattering law parameter (roughness)
-rcs_obs = 25					# The observed radar cross section
-step_ang = 45					# Angle interval (e.g., 90 for 4 orientations)
-P = 321.1                         # Rotation period in seconds
-wl = 0.035                      # Observation wavelength
-Shadowing = False				# Shadowing (recommended only for significantly concave shapes)
-PrintAll = False				# Print all information for every orientation
-MakePlots = False				# Plot the cross sections and albedos for every orientation
+delta = 0                # subradar latitude ([-90,90])
+ObsEcLat = 35.0          # Asteroid's observer-centered ecliptic latitude
+ObsEcLon = 324.0         # Asteroid's observer-centered ecliptic longitude
+PoleEcLat = -44.3        # Pole ecliptic latitude in degrees
+PoleEcLon = 35.6         # Pole ecliptic longitude in degrees
+x0 = 10                  # Map dimension (number of pixels per side)
+law = 'Cos'	             # Scattering law: 'Cos'/'Hag' for Cosine/Hagfors
+C = 0.77                 # scattering law parameter (roughness)
+rcs_obs = 25             # The observed radar cross section
+step_ang = 45            # Angle interval (e.g., 90 for 4 orientations)
+P = 321.1                # Rotation period in seconds
+wl = 0.035               # Observation wavelength
+Shadowing = False        # Shadowing (recommended only for significantly concave shapes)
+PrintAll = False         # Print all information for every orientation
+MakePlots = False        # Plot the cross sections and albedos for every orientation
 ##### END INPUT #####
 
 
